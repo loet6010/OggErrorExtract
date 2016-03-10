09bf89db6fc66398263d016350d6b235c96d21ae
mc-liurh <liurh@mchz.com.cn>
2016-03-10 10:44:53 +0800

    Revert "测试类的更改"
    This reverts commit f6820a9bb725b6681217fd53b17c5770e4d8ed57.

--- src/com/oggerror/test/CreateTbsSpaceTest.java
+++ src/com/oggerror/test/CreateTbsSpaceTest.java
@@ -6,8 +6,8 @@ public class CreateTbsSpaceTest {
 
 	public static void main(String[] args) {
 		CreateTbsSpaceLogic cLogic = new CreateTbsSpaceLogic();
-		String tbs_name = "ET_TEST";
-		if (cLogic.createTbsSpace(tbs_name)) {
+		
+		if (cLogic.createTbsSpace("ET_TEST")) {
 			System.out.println("新建表空间成功");
 		} else {
 			System.out.println("新建表空间失败");
